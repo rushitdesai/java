eb9c822ee9f57cc3263668cabada843680394599
rushit <rndesai.great@gmail.com>
2017-02-19 20:16:38 +0530

    Initial Import

---
+++ core/src/main/java/com/javandotherthings/java/core/App.java
@@ -0,0 +1,13 @@
+package com.javandotherthings.java.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
